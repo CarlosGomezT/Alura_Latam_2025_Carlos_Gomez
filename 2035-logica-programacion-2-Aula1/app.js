3434074ce317f61693087ecc5fd9cac1094a38fe
Carlos Gomez <carlosyoshy10@gmail.com>
2025-02-25 23:36:03 -0700

    Desafio 2-3

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = crearNumeroAleatorio(); 
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElemento (elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -6,47 +7,76 @@ function asignarTextoElemento (elemento, texto){
 }
 
 function crearNumeroAleatorio(){
-    return Math.floor(Math.random()*15)+1;
+    return Math.floor(Math.random()*5)+1;
 }
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value)
-    console.log(typeof(numeroDeUsuario));
-    console.log(numeroDeUsuario);
-    console.log(numeroSecreto);
-    console.log(numeroDeUsuario === numeroSecreto);
+    if(numeroDeUsuario === numeroSecreto)
+    {
+        asignarTextoElemento(`p`, `¡Adivinaste! El numero era ${numeroSecreto} Acertaste en ${intentos} ${intentos===1 ?`intento`:`intentos`}`);
+        document.getElementById(`reiniciar`).removeAttribute(`disabled`);
+    }else{ 
+        if (numeroDeUsuario > numeroSecreto)
+            { 
+                asignarTextoElemento(`p`,`El numero secreto es menor al introducido ${numeroDeUsuario}`);
+            }else
+            {
+                asignarTextoElemento(`p`,`El numero secreto es mayor al introducido ${numeroDeUsuario}`);
+            }
+            intentos++
+            limpiarCaja();
+    } 
     return;
 }
-asignarTextoElemento('h1','Juego del numero secreto');
-asignarTextoElemento('p','Indica un numero del 1 al 15');
 
+function limpiarCaja() {
+    document.getElementById(`valorUsuario`).value=``;
+}
 
-//////////////////////
-//Desafios
-//function holaMundo() {
-//    console.log("Hola undo")
-//}
-//
-//function holaNombre(nombre) {
-//    console.log("Hola " + nombre);
-//}
-//
-//function numeroDoble(numero) {
-//    return numero*2; 
-//}
-//
-//function numerosPromedio (numero1, numero2, numero3)
-//{
-//    return (numero1+numero2+numero3)/3
-//}
-//
-//function numeroMayor(numero1, numero2)
-//{
-//    if (numero1>numero2)
-//    { return numero1
-//    }else {return numero2}
-//}
-//
-//function numeroAlCuadrado(numeroAMultiplicar){
-//    return numeroAMultiplicar*numeroAMultiplicar
-//}
+function reiniciarJuego(){
+    limpiarCaja();
+    condicionesIniciales();
+    document.getElementById(`reiniciar`).setAttribute(`disabled`,`true`);
+}
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del numero secreto');
+    asignarTextoElemento('p','Indica un numero del 1 al 5');
+    intentos = 1;
+    numeroSecreto = crearNumeroAleatorio();
+}
+condicionesIniciales();
+
+//Desafio 2-3
+///////
+function calculoIMC(altura, peso) {
+    let imc = peso/(altura*altura);
+    return peso/altura;
+}
+
+function calculoFatorial (numero) {
+    let conteo = numero
+    let total = numero
+    while (conteo>1)
+    {     
+        total = total*(conteo-1);
+        conteo--;  
+    }
+    return total;
+}
+function convertirAPeso(dolares){
+    pesos = dolares*20.49;
+    return pesos;
+}
+function medirSala(altura, anchura) {
+    console.log(`La sala tiene un perimetro de ${(altura+anchura)*2} y un area de ${altura*anchura}`);    
+}
+function medirSalaCircular(radio) {
+    console.log(`La sala tiene un perimetro de ${radio*2*3.14} y un area de ${radio*radio*3.14}`);   
+}
+function tablaDeMultiplicar(numeroAMultiplicar){
+    for(i=1;i<=10; i++ )
+        {
+            console.log(`${numeroAMultiplicar} x ${i} = ${numeroAMultiplicar*i}`);
+        }  
+}
