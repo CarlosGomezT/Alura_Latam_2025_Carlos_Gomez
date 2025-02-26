3c0d977cd577d77133117bf505745a537ecf9c32
Carlos Gomez <carlosyoshy10@gmail.com>
2025-02-25 21:36:59 -0700

    Curso 2-2 con desafio

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,10 +1,52 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del numero secreto";
+let numeroSecreto = crearNumeroAleatorio(); 
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Seleccione un numero del 1 a 10";
-
-function intentoDeUsuario()
-{
-    alert('Click');
+function asignarTextoElemento (elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
 }
+
+function crearNumeroAleatorio(){
+    return Math.floor(Math.random()*15)+1;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value)
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroDeUsuario);
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
+}
+asignarTextoElemento('h1','Juego del numero secreto');
+asignarTextoElemento('p','Indica un numero del 1 al 15');
+
+
+//////////////////////
+//Desafios
+//function holaMundo() {
+//    console.log("Hola undo")
+//}
+//
+//function holaNombre(nombre) {
+//    console.log("Hola " + nombre);
+//}
+//
+//function numeroDoble(numero) {
+//    return numero*2; 
+//}
+//
+//function numerosPromedio (numero1, numero2, numero3)
+//{
+//    return (numero1+numero2+numero3)/3
+//}
+//
+//function numeroMayor(numero1, numero2)
+//{
+//    if (numero1>numero2)
+//    { return numero1
+//    }else {return numero2}
+//}
+//
+//function numeroAlCuadrado(numeroAMultiplicar){
+//    return numeroAMultiplicar*numeroAMultiplicar
+//}
