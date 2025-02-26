092a2b3bc8f861147dd00b27b8b8487e64abb306
Carlos Gomez <carlosyoshy10@gmail.com>
2025-02-25 19:54:23 -0700

    Initial commit

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego del numero secreto";
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Seleccione un numero del 1 a 10";
+
+function intentoDeUsuario()
+{
+    alert('Click');
+}
